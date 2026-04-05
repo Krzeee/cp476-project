1ba05fd4bd8e5581c0edecf6376061b1898510d4
Krzee <derekhunziker@gmail.com>
2026-04-05 12:10:04 -0400

    theme changer
    Added a button to swap between light and dark mode

--- src/script.js
+++ src/script.js
@@ -331,4 +331,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
     updateJoinLeaveButtonText();
   }
+  // -----------------------
+  // THEME TOGGLE
+  // -----------------------
+  const toggleBtn = document.getElementById("themeToggle");
+
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    toggleBtn.textContent = "🌙";
+  }
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+      localStorage.setItem("theme", "dark");
+      toggleBtn.textContent = "🌙";
+    } else {
+      localStorage.setItem("theme", "light");
+      toggleBtn.textContent = "☀️";
+    }
+  });
 });
